b7661b1651cfc8627f6d63686b17c82ad9630f29
Bond, Noah <Noah.Bond@tdameritrade.com>
2019-02-18 18:26:25 -0500

    Updated ios launch screen, app name, and app icon.  Got MVP proven for ios.  Added cocoapods to ios project.

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -1,14 +1,33 @@
 import UIKit
 import app
+import MaterialComponents
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MainContractView {
+    
+    @IBOutlet weak var weightField: MDCTextField!
+    
+    let presenter = MainPresenter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = Proxy().proxyHello()
+        presenter.view = self
+//        weightField.text = Proxy().proxyHello()
+        weightField.addTarget(self, action: #selector(ViewController.textFieldDidChange(_:)), for: UIControlEvents.editingChanged)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    @IBOutlet weak var label: UILabel!
+    
+    func openSettings() {
+        print("Open Settings")
+    }
+    
+    func displayWeight() {
+        print("Display Weight")
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        presenter.handleWeightInput(weight: textField.text!)
+    }
 }
